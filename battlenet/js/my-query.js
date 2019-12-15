c89996ff1027a6d2b4fe401c38f7bdff6defc1cd
Baistan <tashkulov.xx@gmail.com>
2019-12-15 12:44:26 +0600

    Battlenet

---
+++ battlenet/js/my-query.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+//hamburger
+$('#hamburger').click(function() {
+  $('.main-menu').toggleClass('menu-show');
+  $('.mobile-toggle').toggleClass('active');
+});
+
+});
